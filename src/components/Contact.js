0a0350f43ab4cb307da390ac1bdf7260af7843b6
pablosantos21 <pablosantosbay@gmail.com>
2022-02-23 17:34:37 +0100

    change email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
       <div>
         <p css={pCSS}>INFO: <a href='tel:+34644710447'>644 710 447</a></p>
         <p css={pCSS}>PEDIDOS: <a href='tel:+34644710447'>644 710 447</a></p>
-        <p css={pCSS}>MAIL: <a href='mailto:kuruttacouriers@gmail.com'>kuruttacouriers@gmail.com</a></p>
+        <p css={pCSS}>MAIL: <a href='mailto:kurutta.couriers.vlc@gmail.com' >kurutta.couriers.vlc@gmail.com</a></p>
       </div>
       <div>
         <iframe src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d24630.511782395417!2d-0.378569!3d39.496115!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb87a0eccee7a1ce2!2sMensajer%C3%ADa%20urgente%20Kurutta%20Couriers%20Valencia!5e0!3m2!1ses!2ses!4v1634492657131!5m2!1ses!2ses' css={css`width:600px;height:450px;border:0`} allowFullScreen='' loading='lazy' />
